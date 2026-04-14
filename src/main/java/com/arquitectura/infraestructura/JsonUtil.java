ecdb40736c28e989d71af7839c61c591cc1d0d40
Camilo <camilolon6@gmail.com>
2026-04-13 21:21:01 -0500

    agregar JsonUtil para serializacion de mensajes

---
+++ src/main/java/com/arquitectura/infraestructura/JsonUtil.java
@@ -0,0 +1,33 @@
+package com.arquitectura.infraestructura;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JsonUtil {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    static {
+        mapper.registerModule(new JavaTimeModule());
+    }
+
+    public static String toJson(Object obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializando", e);
+        }
+    }
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException("Error deserializando", e);
+        }
+    }
+
+    public static <T> T convert(Object obj, Class<T> clazz) {
+        return mapper.convertValue(obj, clazz);
+    }
+}
