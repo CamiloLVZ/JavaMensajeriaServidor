813930a6228dc5083053eaf2a864a96e2e8f761a
Angel Garces <angellgarces1024@gmail.com>
2026-04-21 00:00:10 -0500

    ver mensajes

--- src/main/java/com/arquitectura/aplicacion/router/MensajeRouterFactory.java
+++ src/main/java/com/arquitectura/aplicacion/router/MensajeRouterFactory.java
@@ -3,6 +3,8 @@ package com.arquitectura.aplicacion.router;
 import com.arquitectura.mensajeria.enums.Accion;
 import com.arquitectura.dominio.handlers.ConectarHandler;
 import com.arquitectura.dominio.handlers.EnviarArchivoHandler;
+import com.arquitectura.dominio.handlers.ListarDocumentosHandler;
+import com.arquitectura.dominio.handlers.ListarMensajesHandler;
 import com.arquitectura.dominio.handlers.MensajeTextoHandler;
 
 public class MensajeRouterFactory {
@@ -13,6 +15,8 @@ public class MensajeRouterFactory {
         router.registrarHandler(Accion.CONECTAR, new ConectarHandler());
         router.registrarHandler(Accion.ENVIAR_DOCUMENTO, new EnviarArchivoHandler());
         router.registrarHandler(Accion.ENVIAR_MENSAJE, new MensajeTextoHandler());
+        router.registrarHandler(Accion.LISTAR_MENSAJES, new ListarMensajesHandler());
+        router.registrarHandler(Accion.LISTAR_DOCUMENTOS, new ListarDocumentosHandler());
 
         return router;
     }
